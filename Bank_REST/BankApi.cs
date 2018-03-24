e723e582a8a8003b87dffe5dd287d72a36422f30
MCCshreyas <shreyasjejurkar123@live.com>
2018-03-24 20:42:06 +0530

    Added GetCustomerUserName method

--- Bank_REST/BankApi.cs
+++ Bank_REST/BankApi.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Net.Security;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -8,11 +10,38 @@ namespace Bank_REST
     {
 	    static void Main(string[] args)
 	    {
-		    
+		    GetCustomerUserName("653546546");
+
+	    }
+
+	    public static string GetCustomerUserName(string accountNumber = "")
+	    {
+		    if (accountNumber == "") return null;
+		    try
+		    {
+			    using (var client = new WebClient())
+			    {
+				    var jsonResponse = client.DownloadString($"http://localhost:3000/customer/{accountNumber}");
+				    var obj = JsonConvert.DeserializeObject<JArray>(jsonResponse);
+
+				    if (obj.Count == 0)
+				    {
+					    return null;
+				    }
+
+				    var usernameResponse = obj[0]["Name"].ToString();
+				    return usernameResponse.ToString();
+			    }
+		    }
+		    catch (WebException)
+		    {
+			    return null;
+		    }
+		    return null;
 	    }
 		
 
-        public static bool CheckCustomerAuthentication(string accountNumber = "", string password = "")
+        public static bool AuthenticateCustomer(string accountNumber = "", string password = "")
         {
 			try
 			{
