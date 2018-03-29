1c188582e3e70ce49625e1f166e27a7b55f16149
MCCshreyas <shreyasjejurkar123@live.com>
2018-03-29 19:27:20 +0530

    Converted method to protected

--- Bank_REST/Constants.cs
+++ Bank_REST/Constants.cs
@@ -17,7 +17,7 @@
 		/// </summary>
 		/// <param name="id">Customer ID </param>
 		/// <returns></returns>
-		public static string GetCustomerById(string id = null)
+		protected static string GetCustomerById(string id = null)
 		{
 			return id != null ? $@"http://{ServerName}:{PortNumber}/customer/{id}" : null;
 		}
@@ -28,74 +28,79 @@
 		/// <param name="id">Customer ID</param>
 		/// <param name="name">Customer updated name</param>
 		/// <returns></returns>
-		public static string UpdateCustomerNameById(string id = null, string name = null)
+		protected static string UpdateCustomerNameById(string id = null, string name = null)
 		{
 			if (id != null && name != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updatename/{id}/{name}";
 			return null;
 		}
 
-		public static string UpdateCustomerAddressById(string id = null, string address = null)
+		protected static string UpdateCustomerAddressById(string id = null, string address = null)
 		{
 			if (id != null && address != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updateaddress/{id}/{address}";
 			return null;
 		}
 
-		public static string UpdateCustomerPhoneNumberById(string id = null, string phonenumber = null)
+		protected static string UpdateCustomerPhoneNumberById(string id = null, string phonenumber = null)
 		{
 			if (id != null && phonenumber != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updatephonenumber/{id}/{phonenumber}";
 			return null;
 		}
 
-		public static string UpdateCustomerEmailById(string id = null, string email = null)
+		protected static string UpdateCustomerEmailById(string id = null, string email = null)
 		{
 			if (id != null && email != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updateemail/{id}/{email}";
 			return null;
 		}
 
-		public static string UpdateCustomerPasswordById(string id = null, string password = null)
+		protected static string UpdateCustomerPasswordById(string id = null, string password = null)
 		{
 			if (id != null && password != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updatepassword/{id}/{password}";
 			return null;
 		}
 
-		public static string UpdateCustomerAccountBalanceById(string id = null, string balance = null)
+		protected static string UpdateCustomerAccountBalanceById(string id = null, string balance = null)
 		{
 			if (id != null && balance != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updatebalance/{id}/{balance}";
 			return null;
 		}
 
-		public static string UpdateCustomerImagePathById(string id = null, string path = null)
+		protected static string UpdateCustomerImagePathById(string id = null, string path = null)
 		{
 			if (id != null && path != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updateImagePath/{id}/{path}";
 			return null;
 		}
 
-		public static string UpdateCustomerGenderById(string id = null, string gender= null)
+		protected static string UpdateCustomerGenderById(string id = null, string gender= null)
 		{
 			if (id != null && gender != null)
-				return $@"http://{ServerName}:{PortNumber}/customer/updateaddress/{id}/{gender}";
+				return $@"http://{ServerName}:{PortNumber}/customer/updategender/{id}/{gender}";
 			return null;
 		}
 
-		public static string UpdateCustomerMobileVerificationById(string id = null, string mobileverification = null)
+		protected static string UpdateCustomerMobileVerificationById(string id = null, string mobileverification = null)
 		{
 			if (id != null && mobileverification != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updatemobileverification/{id}/{mobileverification}";
 			return null;
 		}
 
-		public static string UpdateCustomerBirthDateById(string id = null, string birthdate = null)
+		protected static string UpdateCustomerBirthDateById(string id = null, string birthdate = null)
 		{
 			if (id != null && birthdate != null)
 				return $@"http://{ServerName}:{PortNumber}/customer/updatebirthdate/{id}/{birthdate}";
 			return null;
+		}
+
+		protected static string DeleteCustomerById(string id = null)
+		{
+			return id != null ? $@"http://{ServerName}:{PortNumber}/customer/{id}" : null;
 		}
 	}
 }
