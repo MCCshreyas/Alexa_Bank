86a6b5fe97eea38fc663bfc6b1356a4788880d99
MCCshreyas <shreyasjejurkar123@live.com>
2018-03-29 19:28:06 +0530

    Added withdraw method

---
+++ BankLibrary/Operations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank_REST;
+namespace BankLibrary
+{
+	public class Operations
+	{
+		public static int WithDrawMoney(string accountNumber = null , long amount = 0 )
+		{
+			if (accountNumber!= null && amount != 0)
+			{
+				var responseBalance = BankApi.GetCustomerBalance(accountNumber);
+				if (responseBalance!= null)
+				{
+					var balance = Convert.ToInt64(responseBalance);
+
+					if (balance < amount)
+					{
+						return -2;
+					}
+
+					if (balance > amount)
+					{
+						balance = balance - amount;
+						BankApi.UpdateCustomerBalance(accountNumber, Convert.ToString(balance));
+						return 0;
+					}
+				}	
+			}
+			return -1;
+		}
+	}
+}
