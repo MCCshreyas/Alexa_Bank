45bb3c0e20a318f623822d5d4b4ff55723b20eee
MCCshreyas <shreyasjejurkar123@live.com>
2018-03-30 20:24:33 +0530

    Added functionality for creating account number

--- BankLibrary/Operations.cs
+++ BankLibrary/Operations.cs
@@ -1,10 +1,20 @@
-﻿namespace BankLibrary
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
 {
 	using System;
 	using Bank_REST;
 
 	public class Operations
 	{
+		private static readonly Random Rng = new Random();
+		static void Main(string[] args)
+		{
+			CreateNewAccount();
+		}
+
+
 		public static int WithDrawMoney(string accountNumber = null, long amount = 0)
 		{
 			if (accountNumber != null && amount != 0)
@@ -57,9 +67,34 @@
 			return 0;
 		}
 
+		public static long CreateNewAccount()
+		{
+			long GenerateAccountNumber()
+			{
+				var builder = new StringBuilder();
+				while (builder.Length < 15)
+				{
+					builder.Append(Rng.Next(10).ToString());
+				}
+				return Convert.ToInt64(builder.ToString());
+			}
 
+			var generatedNumber = GenerateAccountNumber();
 
+			List<string> availableAccountNumber = BankApi.GetCustomersAccountNumbers();
 
+			foreach (var number in availableAccountNumber)
+			{
+				if (number == generatedNumber.ToString())
+				{
+					generatedNumber = GenerateAccountNumber();
+				}
+			}
+
+			return generatedNumber;
+		}
+
+		
 
 	}
 }
