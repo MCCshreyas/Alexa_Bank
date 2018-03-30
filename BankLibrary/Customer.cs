201c1c7bde04a9ec26837e2aa89c830cf7a99371
MCCshreyas <shreyasjejurkar123@live.com>
2018-03-30 20:43:01 +0530

    Builded Customer class

--- BankLibrary/Customer.cs
+++ BankLibrary/Customer.cs
@@ -4,10 +4,10 @@ using Bank_REST;
 
 namespace BankLibrary
 {
-	
 	public class Customer
 	{
 		#region Fields
+
 		private string _name;
 		private string _address;
 		private string _phoneNumber;
@@ -19,13 +19,14 @@ namespace BankLibrary
 		private string _gender;
 		private string _birthDate;
 		private string _mobileNotification;
+
 		#endregion
 
 		#region FieldInitialization
 
-		public Customer(string accountNumber)
+		public void InitializeAllFieldsForExistingAccount(string accountNumber)
 		{
-			 _accountNumber = accountNumber;
+			_accountNumber = accountNumber;
 			_name = BankApi.GetCustomerUserName(_accountNumber);
 			_address = BankApi.GetCustomerAddress(accountNumber);
 			_phoneNumber = BankApi.GetCustomerPhoneNumber(accountNumber);
@@ -37,27 +38,20 @@ namespace BankLibrary
 			_mobileNotification = BankApi.GetCustomerMobileVerificationStatus(accountNumber);
 			_birthDate = BankApi.GetCustomerBirthDate(accountNumber);
 		}
-		
+
+
 		public string Name
 		{
-			get => this._name;
+			get => _name;
 
-			set
-			{
-				//value = ;
-				_name = value;
-			}
+			set => _name = value ?? throw new Exception("Name cannot be null");
 		}
 
 
 		public string Address
 		{
 			get => _address;
-			set
-			{
-				value = BankApi.GetCustomerAddress(this._accountNumber);
-				_name = value;
-			}
+			set => _name = value ?? throw new Exception("Address cannot be null");
 		}
 
 		public string PhoneNumber
@@ -65,22 +59,11 @@ namespace BankLibrary
 			get => _phoneNumber;
 			set
 			{
-				value = BankApi.GetCustomerPhoneNumber(this._accountNumber);
+				if (value == null) throw new Exception("Phone number cannot be null");
 
-				if (value == null)
-				{
-					throw new Exception("Phone number cannot be null");
-				}
+				if (value.Length > 10) throw new Exception("Phone number cannot be greater than 10 numbers");
 
-				if (value.Length > 10)
-				{
-					throw new Exception("Phone number cannot be greater than 10 numbers");
-				}
-
-				if (value.Length < 10)
-				{
-					throw new Exception("Phone number cannot be less than 10");
-				}
+				if (value.Length < 10) throw new Exception("Phone number cannot be less than 10");
 
 				_phoneNumber = value;
 			}
@@ -90,18 +73,7 @@ namespace BankLibrary
 		public string BirthDate
 		{
 			get => _birthDate;
-			set
-			{
-				//value = Convert.ToDateTime(BankApi.GetCustomerBirthDate(this._accountNumber));
-				
-				if (value == null)
-				{
-					throw new Exception("Birth date cannot be null");
-				}
-
-				_birthDate = value;
-			}
-
+			set => _birthDate = value ?? throw new Exception("Birth date cannot be null");
 		}
 
 		public string Email
@@ -109,17 +81,13 @@ namespace BankLibrary
 			get => _email;
 			set
 			{
-				value = BankApi.GetCustomerEmail(this._accountNumber);
-
-				bool isEmail = Regex.IsMatch(value, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+				var isEmail = Regex.IsMatch(value,
+					@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",
+					RegexOptions.IgnoreCase);
 				if (isEmail)
-				{
 					_email = value;
-				}
 				else
-				{
 					throw new Exception("Email address is not valid");
-				}
 			}
 		}
 
@@ -128,54 +96,66 @@ namespace BankLibrary
 			get => _password;
 			set
 			{
-				value = BankApi.GetCustomerPassword(this._accountNumber);
-				if (value.Length < 8)
-				{
-					throw new Exception("Password cannot be less than 8 characters");
-				}
+				if (value.Length < 8) throw new Exception("Password cannot be less than 8 characters");
 
 				_password = value;
 			}
 		}
 
-		public string AccountNumber => _accountNumber;
+		public string AccountNumber
+		{
+			get => _accountNumber;
+			set
+			{
+				if (value.Length < 15) throw new Exception("Account number invalid");
+
+				_accountNumber = value;
+			}
+		}
 
 		public string Balance
 		{
 			get => _balance;
 			set
 			{
-				value = BankApi.GetCustomerBalance(this._accountNumber);
+				if (value == null || value.Contains("-")) throw new Exception("Balance cannot be null or negative");
 
-				if (value == null || value.Contains("-"))
-				{
-					throw new Exception("Balance cannot be null or negative");
-				}
-
-				this._balance = value;
+				_balance = value;
 			}
 		}
 
 		public string Image
 		{
 			get => _image;
-			set => _image = BankApi.GetCustomerImagePath(this._accountNumber);
+			set => Image = value ?? throw new Exception("Provide valid path to photo");
 		}
 
 		public string Gender
 		{
 			get => _gender;
-			set => BankApi.GetCustomerGender(this._accountNumber);
+			set
+			{
+				if (value == null) throw new Exception("Gender cannot be null");
+
+				if (value != "Male" || value != "Female" || value != "Other") throw new Exception("Gender invalid");
+
+				_gender = value;
+			}
 		}
 
 		public string Notification
 		{
 			get => _mobileNotification;
-			set => BankApi.GetCustomerMobileVerificationStatus(this._accountNumber);
+			set
+			{
+				if (value == null) throw new Exception("Value cannot be null");
+
+				if (value != "Yes" || value != "No") throw new Exception("Input invalid");
+
+				_mobileNotification = value;
+			}
 		}
 
 		#endregion
-		
-				
 	}
 }
