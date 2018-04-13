8d17cc322419a5a6d3533d0cb8853e04c09ef185
MCCshreyas <shreyasjejurkar123@live.com>
2018-04-13 19:41:26 +0530

    Revert "Small bug fix"
    This reverts commit 36580941ab62cd54259e6f6ce02593156b345eab.

--- BankLibrary/Authentication.cs
+++ BankLibrary/Authentication.cs
@@ -2,12 +2,12 @@
 
 namespace BankLibrary
 {
-	public class Authentication
-	{
-		public static bool Authenticate(string accountNumber = "" , string password = "")
-		{
-			var responsePassword = BankApi.GetCustomerPassword(accountNumber);
-			return responsePassword != null && responsePassword.ToString().Equals(password);
-		}
-	}
+    public class Authentication
+    {
+        public static bool Authenticate(string accountNumber = "" , string password = "")
+        {
+	        var responsePassword = BankApi.GetCustomerPassword(accountNumber);
+	        return responsePassword != null && responsePassword.ToString().Equals(password);
+        }
+    }
 }
