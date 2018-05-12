d5dedf9450d3822632d17b322bd15ceb6d60258e
MCCshreyas <shreyasjejurkar123@live.com>
2018-05-12 21:44:54 +0530

    Added Transfer money logic and dependent method

--- BankLibrary/Operations.cs
+++ BankLibrary/Operations.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Text;
-
-namespace BankLibrary
+﻿namespace BankLibrary
 {
 	using System;
 	using Bank_REST;
+	using System.Collections.Generic;
+	using System.Text;
+
 
 	public class Operations
 	{
 		private static readonly Random Rng = new Random();
-		
+
 		public static int WithDrawMoney(string accountNumber = null, long amount = 0)
 		{
 			if (accountNumber != null && amount != 0)
@@ -47,6 +47,21 @@ namespace BankLibrary
 			return -2;
 		}
 
+		public static int SetCurrentBalance(string accountNumber = "", string amount = "")
+		{
+			if (accountNumber != "" && amount != "")
+			{
+				var balance = BankApi.UpdateCustomerBalance(accountNumber, amount);
+				if (balance)
+				{
+					return 0;
+				}
+			}
+			return -1;
+		}
+
+
+
 		public static int DepositeMoney(string accountNumber = "", long amount = 0)
 		{
 			if (accountNumber != null && amount != 0)
@@ -62,6 +77,68 @@ namespace BankLibrary
 			}
 			return 0;
 		}
+
+		public static int TransferMoney(string sourceAccountNumber = "", string destinationAccountNumber = "", long amount = 0, string userPassword = "")
+		{
+			if (sourceAccountNumber != "" && destinationAccountNumber != "" && amount != 0 && userPassword != "" && amount >= 0)
+			{
+				var isDestinationAccountExits = CheckAccountExistance(destinationAccountNumber);
+
+				if (isDestinationAccountExits)
+				{
+					var sourceAccountBalance = GetCurrentBalance(sourceAccountNumber);
+					if (sourceAccountBalance < amount)
+					{
+						return -3;
+					}
+					else
+					{
+						//source site
+						var srcCurrentBalance = GetCurrentBalance(sourceAccountNumber);
+						srcCurrentBalance = (int)(srcCurrentBalance - amount);
+						SetCurrentBalance(sourceAccountNumber, srcCurrentBalance.ToString());
+						
+						//destination side
+						var destcurrentBalance = GetCurrentBalance(destinationAccountNumber);
+						destcurrentBalance = (int)(destcurrentBalance + amount);
+						SetCurrentBalance(destinationAccountNumber, destcurrentBalance.ToString());
+						return 0;
+					}	
+				}
+				return -2;
+			}
+			return -1;
+		}
+
+		private static bool CheckAccountExistance(string accountNumber = "")
+		{
+			var availableAccountNumberList = BankApi.GetCustomersAccountNumbers();
+			foreach (var account in availableAccountNumberList)
+			{
+				if (account.Equals(accountNumber))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+
+		private static string GetUserPassword(string accountNumber = "")
+		{
+			if (accountNumber != "")
+			{
+				var password = BankApi.GetCustomerPassword(accountNumber);
+				return password;
+			}
+			else
+			{
+				return string.Empty;
+			}
+		}
+
+
 
 		public static long InitializeNewAccount()
 		{
