165d649903b0bc3032e23eec9b22d99dadfbda59
MCCshreyas <shreyasjejurkar123@live.com>
2018-05-29 21:19:39 +0530

    Revert "Revert "Added MySql connection method""
    This reverts commit 304a5edebbcf432731993e570e0b9ad714ba23da.

--- Web_Api_Alexa/Controllers/ValuesController.cs
+++ Web_Api_Alexa/Controllers/ValuesController.cs
@@ -4,36 +4,41 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MySql.Data.MySqlClient;
+using Web_Api_Alexa.Models;
 
 namespace Web_Api_Alexa.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public List<Customer> Get()
         {
-            return new string[] { "value1", "value2" };
+            return null;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return null;
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+
         }
     }
 }
---
+++ Web_Api_Alexa/Models/Customer.cs
@@ -0,0 +1,37 @@
+﻿namespace Web_Api_Alexa.Models
+{
+    public class Customer
+    {
+        public string Name;
+        public string Address;
+        public string Phone_number;
+        public string Email;
+        public string Password;
+        public string Balance;
+        public string Account_number;
+        public string ImagePath;
+        public string Gender;
+        public string BirthDate;
+
+        public Customer()
+        {
+        }
+
+        public Customer(string name, string address, string phoneNumber, string email, string password, string accountBalance, string accountNumber, string imagePath, string gender, string Birth_date)
+        {
+            Name = name;
+            Address = address;
+            Phone_number = phoneNumber;
+            Email = email;
+            Password = password;
+            Balance = accountBalance;
+            Account_number = accountNumber;
+            ImagePath = imagePath;
+            Gender = gender;
+            BirthDate = Birth_date;
+        }
+
+
+
+    }
+}
