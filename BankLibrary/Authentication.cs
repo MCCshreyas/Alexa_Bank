03230243ba26f4688465af312a1cb2516e713b8c
MCCshreyas <shreyasjejurkar123@live.com>
2018-05-29 10:37:32 +0530

    Added try-catch block

--- BankLibrary/Authentication.cs
+++ BankLibrary/Authentication.cs
@@ -1,13 +1,23 @@
-﻿using Bank_REST;
+﻿using System.Diagnostics;
+using Bank_REST;
 
 namespace BankLibrary
 {
-	public class Authentication
-	{
-		public static bool Authenticate(string accountNumber = "" , string password = "")
-		{
-			var responsePassword = BankApi.GetCustomerPassword(accountNumber);
-			return responsePassword != null && responsePassword.ToString().Equals(password);
-		}
-	}
+    public class Authentication
+    {
+        public static bool Authenticate(string accountNumber = "", string password = "")
+        {
+            try
+            {
+                var responsePassword = BankApi.GetCustomerPassword(accountNumber);
+                return responsePassword != null && responsePassword.ToString().Equals(password);
+            }
+            catch (System.Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+
+            return false;
+        }
+    }
 }
