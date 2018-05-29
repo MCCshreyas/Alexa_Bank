d26788b73ed1cd81fb010d0fb9e25fa0c3405137
MCCshreyas <shreyasjejurkar123@live.com>
2018-05-29 10:39:19 +0530

    Added design for MainWindow

--- Alexa_Desktop/MainWindow.xaml.cs
+++ Alexa_Desktop/MainWindow.xaml.cs
@@ -1,15 +1,82 @@
-﻿using System.Windows;
+﻿using BankLibrary;
+using System;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Alexa_Desktop
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_acc_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ForgetAccountNumberHyperLink_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ForgetpasswordHyperLink1_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool DoValidation()
+        {
+            if (TextBoxAcc.Text.Equals(string.Empty) && PasswordBox.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Please enter all fields", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void Button1Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (DoValidation())
+                {
+                    if (Authentication.Authenticate(TextBoxAcc.Text, PasswordBox.Text))
+                    {
+                        MessageBox.Show("Sucess");
+                        Hide();
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Something went wrong ", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Hyperlink_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ButtonGitHub_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ButtonTwitter_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ButtonEmail_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
 }
