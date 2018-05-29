304a5edebbcf432731993e570e0b9ad714ba23da
MCCshreyas <shreyasjejurkar123@live.com>
2018-05-29 21:15:59 +0530

    Revert "Added MySql connection method"
    This reverts commit 096147f6c90e35e6265e160b15b94d50b071bf96.

--- Web_Api_Alexa/Controllers/ValuesController.cs
+++ Web_Api_Alexa/Controllers/ValuesController.cs
@@ -4,41 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using MySql.Data.MySqlClient;
-using Web_Api_Alexa.Models;
 
 namespace Web_Api_Alexa.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
-        public List<Customer> Get()
+        public IEnumerable<string> Get()
         {
-            return null;
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return null;
+            return "value";
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
-
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
-
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
-
         }
     }
 }
