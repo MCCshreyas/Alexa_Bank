680e1453f74038acbd9b6ca3f46582b6710be900
MCCshreyas <shreyasjejurkar123@live.com>
2018-06-08 23:30:10 +0530

    Added Customer & Authentication class

--- BankLibrary/Authentication.cs
+++ BankLibrary/Authentication.cs
@@ -1,20 +1,56 @@
-﻿using System.Diagnostics;
-using Bank_REST;
+﻿using Bank_REST;
+using Alexa_Server;
 
-namespace BankLibrary
+// ReSharper disable once CheckNamespace
+namespace Alexa_API
 {
     public class Authentication
     {
-        public static bool Authenticate(string accountNumber = "", string password = "")
+      
+        public static int Authenticate(string accountNumber = "", string password = "")
         {
-            try
+            if (accountNumber != "" && password != "")
             {
-                var responsePassword = BankApi.GetCustomerPassword(accountNumber);
-                return responsePassword != null && responsePassword.ToString().Equals(password);
+                var availability = IsAccountExists(accountNumber);
+
+                if (availability)
+                {
+                    var responsePassword = AlexaService.GetPassword(accountNumber);
+
+                    if (responsePassword.Equals(password))
+                    {
+                        return 0;
+                    }
+
+                    return -1;
+                }
+
+                return -2;
+
+
             }
-            catch (System.Exception e)
+
+            return -3;
+        }
+        
+        public static bool IsAccountExists(string accountNumber = "")
+        {
+            if (accountNumber != "")
             {
-                Debug.WriteLine(e);
+                var allAccountNumber = AlexaService.GetAllAccountNumber();
+
+                foreach (var acc in allAccountNumber)
+                {
+                    if (acc.Equals(accountNumber))
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            else
+            {
+                return false;
             }
 
             return false;
