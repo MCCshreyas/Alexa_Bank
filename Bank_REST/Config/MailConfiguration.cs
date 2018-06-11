342c1ecc7bb5c57142bf53d5288e621fb0270a67
MCCshreyas <shreyasjejurkar123@live.com>
2018-06-11 23:37:42 +0530

    Added Configuration classes for Mail and Server

---
+++ Bank_REST/Config/MailConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mail;
+
+namespace Bank_REST.Config
+{
+    /// <summary>
+    /// Setting for Mail 
+    /// </summary>
+    public class MailConfiguration
+    {
+        protected static string MailAddress = @"shreyasjejurkar123@live.com";
+
+
+        protected static string MailSubject = "Alexa Bank account confirmation mail";
+
+        protected static SmtpClient SetUpMailServer()
+        {
+            var smtp = new SmtpClient
+            {
+                Host = "smtp-mail.outlook.com",
+                Port = 587,
+                UseDefaultCredentials = false,
+                Credentials = new System.Net.NetworkCredential("shreyasjejurkar123@live.com", "MCCshreyas9970209265"),
+                EnableSsl = true
+            };
+            return smtp;
+        }
+
+
+    }
+}
---
+++ Bank_REST/Config/ServerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Bank_REST.Config
+{
+    class ServerConfiguration
+    {
+        /// <summary>
+        /// Default server name
+        /// </summary>
+        public const string ServerName = "localhost";
+
+        /// <summary>
+        /// Default Website URL
+        /// </summary>
+        public const string Url = "alexabank";
+
+        /// <summary>
+        /// Base address
+        /// </summary>
+        public static string BaseAddress = $"{ServerName}/{Url}";
+    }
+}
